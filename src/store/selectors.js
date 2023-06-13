e13983428b07411a99107e8b6443340ef628cc9d
klav28 <klav28@gmail.com>
2023-06-13 23:56:57 +0300

    Update selectors.js

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -2,8 +2,7 @@ export const getContacts = state => state.phonebook;
 
 export const getFilter = state => state.filter.filter;
 
-export const getFilteredContacts = state => {
-  return getContacts(state).filter(el =>
+export const getFilteredContacts = state =>
+  getContacts(state).filter(el =>
     el.name.toLowerCase().includes(getFilter(state).toLowerCase())
   );
-};
